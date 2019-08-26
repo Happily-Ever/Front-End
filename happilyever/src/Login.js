840bdced9c47c00711e2ef3ac6fc363c7c2bbec6
Solar Flair <solarflair@MacBook-Air.local>
2019-08-26 16:09:07 -0400

    :tada: initial commit & formik structure
    inital commit & formik structure

--- happilyever/src/Login.js
+++ happilyever/src/Login.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { withFormik, Form, Field } from "formik";
 
-export default function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+function nonFormik() {
+  return (
+    <div className="loginregister">
+      <h1>Please Login</h1>
+      <Form>
+        <p>Username</p>
+        <Field 
+            type="text" 
+            name="username"
+            placeholder="No profanity, please"/>
+        <p>Password</p>
+        <Field
+            type="password" 
+            name="password"
+            placeholder="Must contain a numeral"/>
+        <button>Submit</button>
+      </Form>
+    </div>
+  )
 }
+
+const Login = withFormik({
+    mapPropsToValues({username, password}){
+        return {
+            username: username || "",
+            password: password || ""
+        }
+    },
+    handleSubmit(values){
+        console.log(values);
+    }
+})(nonFormik);
+
+export default Login;
