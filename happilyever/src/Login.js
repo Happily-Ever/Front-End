a2596571ba2b49039888e572969cae5b4e0351e2
Solar Flair <solarflair@MacBook-Air.local>
2019-08-26 16:25:45 -0400

    initial commit & formik / yum structure

--- happilyever/src/Login.js
+++ happilyever/src/Login.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function nonFormik() {
+function nonFormik({errors, touched}) {
   return (
     <div className="loginregister">
       <h1>Please Login</h1>
       <Form>
         <p>Username</p>
+        {touched.email && errors.email && <p>{errors.email}</p>}
         <Field 
             type="text" 
             name="username"
             placeholder="No profanity, please"/>
         <p>Password</p>
+        {touched.password && errors.password && <p>{errors.password}</p>}
         <Field
             type="password" 
             name="password"
-            placeholder="Must contain a numeral"/>
+            placeholder="Must be at least 7 characters"/>
         <button>Submit</button>
       </Form>
     </div>
@@ -29,6 +32,14 @@ const Login = withFormik({
             password: password || ""
         }
     },
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+            .email("Must be a traditional email format")
+            .required("Email field is required"),
+        password: Yup.string()
+            .min(7, "Password must be at least 7 characters")
+            .required("Password field is required")
+    })
     handleSubmit(values){
         console.log(values);
     }
