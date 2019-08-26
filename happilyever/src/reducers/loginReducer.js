1834d2d440e4e3e92a664fc8334db267c1759c42
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-26 19:06:17 -0400

    :sparkles: adds login reducer
    adds login reducer

--- happilyever/src/reducers/loginReducer.js
+++ happilyever/src/reducers/loginReducer.js
@@ -1,10 +1,32 @@
 const initialState = {
   isLogged: false,
   user: [],
-  token: localStorage.getItem("token")
+  token: null,
+  error: ""
 };
 
 export const loginReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "LOGIN_USER_START":
+      return {
+        ...state,
+        isLogged: false
+      };
+    case "LOGIN_USER_SUCCESS":
+      return {
+        ...state,
+        isLogged: true,
+        token: localStorage.getItem("token")
+      };
+
+    case "LOGIN_USER_FAILURE":
+      return {
+        ...state,
+        isLogged: false,
+        error: ""
+      };
+
+    default:
+      return state;
   }
 };
