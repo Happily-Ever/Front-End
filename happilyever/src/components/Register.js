0e27496dc4d289004a8851f8e62ef2f795296480
Solar Flair <jon.s.solari@gmail.com>
2019-08-27 18:39:09 -0400

    :lipstick: login and register styles match
    login and register styles match

--- happilyever/src/components/Register.js
+++ happilyever/src/components/Register.js
@@ -2,7 +2,45 @@ import React, {useState, useEffect} from 'react';
 import { withFormik, Form, Field } from "formik";
 import {NavLink} from "react-router-dom";
 import * as Yup from "yup";
-import axios from "axios"
+import axios from "axios";
+import styled from "styled-components";
+
+const LogReg = styled.div`  
+display: flex;
+flex-direction: column;
+margin: 3rem auto;
+padding: 1rem;
+width: 28%;
+background: #00A3FF;
+box-shadow: 0px 0px 24px rgba(0, 163, 255, 0.2);
+border-radius: 20px;
+`
+
+const Formo = styled.div`
+display: flex;
+flex-direction: column;
+text-align: center;
+`
+
+const H1 = styled.h1`
+text-align: center;
+color: white;
+`
+
+const H2 = styled.h2`
+text-align: center;
+color: white;
+`
+
+const Label = styled.label`
+margin-bottom: 1rem;
+color: white;
+`
+
+const Button = styled.button`
+width: 25%;
+margin: 1rem auto;
+`
 
 function NonFormikReg({errors, touched, values, isSubmitting, status}) {
   const [users, addUser] = useState();
@@ -16,28 +54,32 @@ function NonFormikReg({errors, touched, values, isSubmitting, status}) {
 
 
   return (
-    <div className="loginregister">
-      <h1>Please Register</h1>
+    <LogReg>
+      <H1>Please Register</H1>
       <Form>
-        <label>Username
+        <Formo>
+        <Label>Username<br/>
           {touched.email && errors.email && <p>{errors.email}</p>}
           <Field 
             type="text" 
             name="username"
-            placeholder="No profanity, please"/>
-        </label>
-        <label>Password
+            placeholder="No profanity, please"
+            style={{padding: ".5rem", borderRadius: "10px"}}/>
+        </Label>
+        <Label>Password<br/>
         {touched.password && errors.password && <p>{errors.password}</p>}
           <Field
             type="password" 
             name="password"
-            placeholder="Must be at least 7 characters"/>
-        </label>
-        <button disabled={isSubmitting}>Submit</button>
-        <h2>Have an Account?</h2>
-        <NavLink /*to " "*/>Login</NavLink>
+            placeholder="At least 7 characters"
+            style={{padding: ".5rem", borderRadius: "10px"}}/>
+        </Label>
+        <Button disabled={isSubmitting}>Submit</Button>
+        <H2>Have an Account?</H2>
+        <NavLink to="/login" style={{textDecoration: "none", color: "white", fontSize: "2rem", textDecoration: "underline"}}>Login</NavLink>
+        </Formo>
       </Form>
-    </div>
+    </LogReg>
   )
 }
 
