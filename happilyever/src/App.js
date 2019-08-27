35ef9d7579f296107bdf712ab0bde329b1e907dd
Solar Flair <jon.s.solari@gmail.com>
2019-08-27 13:21:24 -0400

    :tada: initial app commit
    initial app commit

--- happilyever/src/App.js
+++ happilyever/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Dummy Component</h1>
     </div>
   );
 }
 
-export default App;
+export default App; 
