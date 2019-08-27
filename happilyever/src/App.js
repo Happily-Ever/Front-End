a0e8c9a4d89f95fa5c43314c46294f2a57498030
Solar Flair <jon.s.solari@gmail.com>
2019-08-27 17:13:59 -0400

    :recycle: converted navbar css to styled-components
    converted navbar css to styled-components

--- happilyever/src/App.js
+++ happilyever/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from "./Login.js";
-import Register from "./Register.js";
-import NavBar from "./NavBar.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <Router>
-      {/* <NavBar/> */}
-      <h1>Dummy Component</h1>
-      <Link to="/login">Login Test</Link><br/>
+      <NavBar/>
       <Link to="/register">Register Test</Link>
       <Route path="/login" component={Login}/>
       <Route path="/register" component={Register}/>
--- happilyever/src/components/NavBar.js
+++ happilyever/src/components/NavBar.js
@@ -2,19 +2,16 @@ import React from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 import Login from "./Login.js";
-import Logo from "./img/logo.png";
+import Logo from "../img/logo.png";
 
 const Bar = styled.div`
-max-width: 1440px;
-width: 100%;
-margin: 20px auto 0 auto;
+margin: 0 auto;
 display: flex;
 justify-content: space-between;
 align-items: center;
 height: 60px;
 background: rgba(0, 163, 255, 0.8);
 padding: 0 20px;
-position: fixed;
 align-content: space-between;
 border: 1px solid rgba(0, 162, 255, 0.945);
 `
@@ -23,6 +20,7 @@ const H1 = styled.h1`
 font-family: 'Tangerine', cursive;
 font-weight: bold;
 font-size: 3rem;
+color: white;
 `
 
 const Nav = styled.nav`
@@ -34,11 +32,13 @@ justify-content: space-between;
 export default function NavBar(){
     return(
         <Bar>
-        <Link to="index.html"><H1>HappilyEver</H1></Link>
-        <img src={Logo} alt={"HappilyEver Logo"}/>
+        <Link to="index.html" style={{textDecoration: "none"}}>
+            <H1>HappilyEver</H1>
+        </Link>
+        <img style={{width: "5rem"}} src={Logo} alt={"HappilyEver Logo"}/>
         <Nav>
-            <Link to="#">About Me</Link>
-            <Link to={Login}>Login</Link>
+            <Link to="#" style={{textDecoration: "none", color: "white"}}>About Me</Link>
+            <Link to="/login" style={{textDecoration: "none", color: "white"}}>Login</Link>
         </Nav>
         </Bar>
     )
