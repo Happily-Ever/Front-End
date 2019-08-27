87910a2d096fd586ecaeb75e92bce436931b093e
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-27 14:18:49 -0400

    :sparkles: adds new reducers to the list of combine reducers
    adds new reducers to the list of combine reducers

--- happilyever/src/reducers/index.js
+++ happilyever/src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from "redux";
 import { regReducer } from "./regReducer";
 import { loginReducer } from "./loginReducer";
 import { logoutReducer } from "./logoutReducer";
+import { weddingReducer } from "./weddingReducer";
+import { deletedWeddingReducer } from "./deleteWeddingReducer";
 
 export default combineReducers({
   regReducer,
   loginReducer,
-  logoutReducer
+  logoutReducer,
+  weddingReducer,
+  deletedWeddingReducer
 });
