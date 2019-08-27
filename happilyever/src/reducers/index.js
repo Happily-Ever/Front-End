655e6f3a5acd591e9e35aef0721bdc9a43be5e59
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-27 12:39:16 -0400

    :sparkles: adds login reducer and logout reducer to combine reducer
    adds login reducer and logout reducer to combine reducer

--- happilyever/src/reducers/index.js
+++ happilyever/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { regReducer } from "./regReducer";
 import { loginReducer } from "./loginReducer";
+import { logoutReducer } from "./logoutReducer";
 
 export default combineReducers({
   regReducer,
-  loginReducer
+  loginReducer,
+  logoutReducer
 });
