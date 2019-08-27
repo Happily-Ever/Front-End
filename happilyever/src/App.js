eca335400e4d6dfcb38c06e5f8757d25fc15d8e3
Solar Flair <jon.s.solari@gmail.com>
2019-08-27 15:15:56 -0400

    :sparkles: added router link tests
    added router link tests

--- happilyever/src/App.js
+++ happilyever/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./Login.js ";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./Login.js";
 import Register from "./Register.js";
 
 function App() {
   return (
     <Router>
       <h1>Dummy Component</h1>
+      <Link to="/login">Login Test</Link><br/>
+      <Link to="/register">Register Test</Link>
       <Route path="/login" component={Login}/>
       <Route path="/register" component={Register}/>
     </Router>
