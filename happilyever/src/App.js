c9e68e80a26ead4ba952ab6c8ec953107d053a3c
Tgleason1980 <terryscoding@gmail.com>
2019-08-28 10:55:18 -0500

    started new wedding form

--- happilyever/src/App.js
+++ happilyever/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WeddingForm from './WeddingForm';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <WeddingForm/>
     </div>
   );
 }
--- happilyever/src/WeddingForm.js
+++ happilyever/src/WeddingForm.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { withFormik, Form, Field} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-export default function WeddingForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+function FormikWeddingForm({ values, errors, touched, isSubmitting }) {
+  return (
+      
+    <Form>
+      <div>
+        {touched.bridesname && errors.bridesname && <p>{errors.bridesname}</p>}
+        <Field type="text" name="Brides name" placeholder="Brides name" />
+      </div>
+      <div>
+        {touched.groomsname && errors.groomsname && <p>{errors.groomsname}</p>}
+        <Field type="text" name="Grooms name" placeholder="Grooms name" />
+      </div>
+      
+      <button type="submit" disabled={isSubmitting}>Login &rarr;</button>
+    </Form>
+    
+  );
 }
+
+const WeddingForm = withFormik({
+  mapPropsToValues({ bridesname, groomsname }) {
+    return {
+      bridesname: bridesname || "",
+      groomsname: groomsname || "",
+      
+    };
+  },
+  validationSchema: Yup.object().shape({
+      bridesname: Yup.string()
+      .required("bridesname is required"),
+    groomsname: Yup.string()
+      .required("groomsname is required")
+  }),
+//   handleSubmit(values, formikBag) {
+//     //console.log(formikBag);
+//     if (values.bridesname === "alreadytaken@atb.dev") {
+//       formikBag.setErrors({ bridesname: "That bridesname is already taken" });
+//     } else {
+//       axios
+//         .post("", values)
+//         .then(res => {
+//           localStorage.setItem('token', res.data.token);
+//           formikBag.props.history.push('');
+//           console.log(res);
+//           formikBag.resetForm();
+//           formikBag.setSubmitting(false);
+
+//         })
+//         .catch(err => {
+//           console.log(err); 
+//         });
+    //}
+  //}
+})(FormikWeddingForm);
+ 
+export default WeddingForm;
