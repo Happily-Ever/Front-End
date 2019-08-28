93d0f56e2812ed32ee5598c69f024d0d40812a05
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-28 13:52:21 -0400

    :sparkles: adds welcome page styles
    adds welcome page styles

--- happilyever/src/App.js
+++ happilyever/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from "./Login.js";
-import Register from "./Register.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register.js";
 import NavBar from "./NavBar.js";
+import WelcomePage from "./components/WelcomePage";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
-    <Router>
-      {/* <NavBar/> */}
-      <h1>Dummy Component</h1>
-      <Link to="/login">Login Test</Link><br/>
-      <Link to="/register">Register Test</Link>
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        <NavBar />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route exact path="/" component={WelcomePage} />
+      </Router>
+    </>
   );
 }
 
-export default App; 
+export default App;
---
+++ happilyever/src/components/WelcomePage.js
@@ -0,0 +1,108 @@
+import React from "react";
+// Image Imports
+import hero from "../img/hero.png";
+import rings from "../img/rings.png";
+import couples from "../img/couples.png";
+// Css Imports
+import "../../src/welcomePage.css";
+// Material Ui Imports
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+//Material Ui UseStyles
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    marginTop: "50px",
+    margin: "0 auto",
+    background: "#00A3FF"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  },
+  grid: {
+    marginTop: "50px"
+  }
+});
+
+const WelcomePage = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Paper
+        component="img"
+        src={hero}
+        elevation="20"
+        width="100%"
+        height="400px"
+        square="true"
+      />
+      <div className="box">
+        <h4>Welcome, Wedding Planner !</h4>
+      </div>
+      <Container>
+        <Grid container spacing={1} className={classes.grid}>
+          <Grid item lg={6}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="h2"
+                    align="center"
+                    style={{ color: "#fff" }}
+                  >
+                    Add a Wedding
+                  </Typography>
+                  <img
+                    src={rings}
+                    style={{
+                      marginLeft: "90px",
+                      height: "125px",
+                      paddingTop: "20px"
+                    }}
+                  />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item lg={6}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="h2"
+                    align="center"
+                    style={{ color: "#fff" }}
+                  >
+                    View Couples
+                  </Typography>
+                  <img
+                    src={couples}
+                    style={{ marginLeft: "90px", paddingTop: "25px" }}
+                  />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default WelcomePage;
