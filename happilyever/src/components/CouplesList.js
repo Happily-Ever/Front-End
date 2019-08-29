972e37a8b697438dfa3e271de3f0d9ff915f67f2
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-29 17:24:38 -0400

    :sparkles: adds grid from material ui
    adds grid from material ui

--- happilyever/src/components/CouplesList.js
+++ happilyever/src/components/CouplesList.js
@@ -1,43 +1,77 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
 import CoupleCard from "./CoupleCard.js";
-import styled from "styled-components"
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { weddingsList } from "../actions";
+import Grid from "@material-ui/core/Grid";
+import LoadingScreen from "react-loading-screen";
+import couples from "../img/couples.png";
 
 const DivWed = styled.div`
-margin: 1rem auto;
-width: 50%;
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-justify-content: flex-start;
-`
+  margin: 1rem auto;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`;
 
 const DivCard = styled.div`
-margin: 2rem;
-`
+  margin: 2rem;
+`;
 
-export default function CouplesList() {
-    
-const [couple, addCouple] = useState([]);
+function CouplesList(props) {
+  useEffect(() => {
+    props.weddingsList();
+  }, []);
 
-useEffect(()=>{
-    axios.get("https://lambda-wedding-planner.herokuapp.com/api/posts/all")
-    .then(function(response){
-        addCouple(response.data);
-    })}, []);
-return(
-    <DivWed>
-    {couple.map(couple => {
-      return(
-<DivCard>
-      <CoupleCard 
-        key={couple.id}
-        pic={couple.item_photo}
-        couple_name={couple.couple_name}
-        wedding_date={couple.wedding_date}
-        wedding_theme={couple.wedding_theme}
-        wedding_location={couple.wedding_location}
-    />
-    </DivCard>)})}
+  if (!props.weddings) {
+    return (
+      <LoadingScreen
+        loading={true}
+        bgColor="#f1f1f1"
+        spinnerColor="#9ee5f8"
+        textColor="#676767"
+        logoSrc={couples}
+        text="Loading Weddings"
+      />
+    );
+  }
+
+  return (
+    <Grid container spacing={12}>
+      <DivWed>
+        {props.weddings.map(wedding => {
+          return (
+            <Grid item={6}>
+              <DivCard>
+                <CoupleCard
+                  key={wedding.id}
+                  pic={wedding.item_photo}
+                  couple_name={wedding.couple_name}
+                  wedding_date={wedding.wedding_date}
+                  wedding_theme={wedding.wedding_theme}
+                  wedding_location={wedding.wedding_location}
+                />
+              </DivCard>
+            </Grid>
+          );
+        })}
       </DivWed>
-)};
+    </Grid>
+  );
+}
+
+const mapStateToProps = state => {
+  console.log("STATE IN COUPLES LIST", state.weddingReducer.weddings);
+  return {
+    weddings: state.weddingReducer.weddings,
+    isLoading: state.weddingReducer.isLoading
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { weddingsList }
+)(CouplesList);
