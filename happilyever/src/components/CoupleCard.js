4aceca93fad4e97d85e8105ea04053c4b91fdbfa
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-08-29 17:22:49 -0400

    :sparkles: adds grid to couple card
    adds grid to couple card

--- happilyever/src/components/CoupleCard.js
+++ happilyever/src/components/CoupleCard.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
+import Grid from "@material-ui/core/Grid";
 
-const CoupleCard = (props) => (
-  <Card>
-    <Image src={props.pic} wrapped ui={false}/>
-    <Card.Content>
-      <Card.Header>{props.couple_name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>Married on {props.wedding_date}</span>
-      </Card.Meta>  
-      <Card.Description>
-        {props.wedding_theme}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <div>
-        <Icon name='globe' />
-        Location: {props.wedding_location}
-      </div>
-    </Card.Content>
-  </Card>
-)
+const CoupleCard = props => (
+  <Grid spacing={5} container>
+    <Card>
+      <Image src={props.pic} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.couple_name}</Card.Header>
+        <Card.Meta>
+          <span className="date">Married on {props.wedding_date}</span>
+        </Card.Meta>
+        <Card.Description>{props.wedding_theme}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div>
+          <Icon name="globe" />
+          Location: {props.wedding_location}
+        </div>
+      </Card.Content>
+    </Card>
+  </Grid>
+);
 
 export default CoupleCard;
